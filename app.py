96de1290652f13b5f3a21552d67e4a5a82b190d9
Glenn Axelrod <gsacom@verizon.net>
2013-11-09 21:52:27 -0500

    Upload a file.

--- app.py
+++ app.py
@@ -1,10 +1,83 @@
-from flask import Flask
+#!/usr/bin/python
+
+import os
+from flask import Flask, request, redirect, url_for, render_template
+from werkzeug import secure_filename
+import artists
+
+
+UPLOAD_FOLDER = '~/dev/Collector'
+ALLOWED_EXTENSIONS = set(['xml'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
+
+
+"""
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+"""		
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+       	mybuffer = file.readlines()
+        return str(mybuffer)
+        """
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+ 		"""
+ 
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+from flask import send_from_directory
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+	
+
+"""
+@app.route("/")
 def index():
-    return "<h1> This app is running! </h1><p>Hi Glenn</p>"
+	#return "<h1> Hello World! </h1>"
+	return render_template("index.html")
+"""
+"""
+@app.route("/")
+def check_template():
+	return render_template("index.html")
+"""
+"""
+@app.route("/xmlfile", methods='POST')
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+
+"""
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='127.0.0.1', port=port, debug=True)
