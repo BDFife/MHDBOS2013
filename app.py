79c05d2b46bebead8d5ad95c2cb12f2ddfafaf1a
Brian Fife <bdfife@gmail.com>
2013-11-09 22:59:23 -0500

    Cleaned up some unused content on app.py

--- app.py
+++ app.py
@@ -6,22 +6,10 @@ from werkzeug import secure_filename
 import artists
 
 
-UPLOAD_FOLDER = '~/dev/Collector'
+UPLOAD_FOLDER = '~/Projects/Collector'
 ALLOWED_EXTENSIONS = set(['xml'])
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-
-
-"""
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-"""		
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -29,14 +17,6 @@ def upload_file():
         file = request.files['file']
        	mybuffer = file.readlines()
         return str(mybuffer)
-        """
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
- 		"""
- 
     return '''
     <!doctype html>
     <title>Upload new File</title>
@@ -46,36 +26,11 @@ def upload_file():
          <input type=submit value=Upload>
     </form>
     '''
-
-from flask import send_from_directory
-
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-	return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 	
-
-"""
-@app.route("/")
-def index():
-	#return "<h1> Hello World! </h1>"
-	return render_template("index.html")
-"""
 """
 @app.route("/")
 def check_template():
 	return render_template("index.html")
-"""
-"""
-@app.route("/xmlfile", methods='POST')
-def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-
 """
 
 if __name__ == '__main__':
